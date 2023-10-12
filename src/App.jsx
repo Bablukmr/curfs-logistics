b8afd90c7492ea51bbbcb4416d266d19b4e51fc1
Bablukmr <bablukmr800@gmail.com>
2023-10-12 12:41:18 +0530

    Hi

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>Name:  </p>
+        <p>Na:  </p>
         <p>Hello sir </p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
