b3f4f352068553882edbd864f3c8bb9ffa907ac0
Bablukmr <bablukmr800@gmail.com>
2023-10-12 16:59:05 +0530

    Tab

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LogInEmail from './LogInEmail'
 import Logo from '../assets/Group18.png'
+import LonInPincode from './LonInPincode'
 
 function LogIn() {
+const [tab,setTab]=useState(true)
+
   return (
-    <div className="w-full  h-full flex flex-col items-center justify-center">
+    <div className="w-full  h-full flex flex-col items-center justify-center ">
     <div className="bg-[#34398C] w-full h-[20vh] flex items-center justify-center">
       <img src={Logo} width={250} alt='LOGO' />
     </div>
-    <LogInEmail/>
+    
+         {tab ? 
+                 <LogInEmail/>
 
-    <div className="absolute bottom-0">Tab</div>
+                 :<LonInPincode/>
+
+    
+          }
+  
+    <div className="absolute w-full bottom-0 border-t-2  flex">
+        <div onClick={()=>setTab(true)} className={`w-[50%] h-[60px] flex items-center justify-center transition-all duration-500 
+        ${tab ? "border-[#2B3087] border-t-[4px] bg-[#EDECEC]" :" "}
+        `}><h2>E-mail</h2></div>
+        <div onClick={()=>setTab(false)} className={`w-[50%] h-[60px] flex items-center justify-center transition-all duration-500 
+        ${tab ? " ": "border-[#2B3087] border-t-[4px] bg-[#EDECEC]" }`}><h2>Pincode</h2></div>
+
+    </div>
   </div>
   )
 }
